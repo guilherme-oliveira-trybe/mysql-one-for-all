5a71bf857f81a3a41d4710f16be168691f6f9740
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-07-25 12:59:13 -0300

    feat: Busca usuários ativos em 2021 com base na data mais recente de reprodução informado na tabela de histórico de repoducões - Requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT
+  u.nome_usuario AS 'usuario',
+  IF(YEAR(MAX(h.data_reproducao)) >= 2021 , 'Usuário ativo', 'Usuário inativo') AS 'condicao_usuario'
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h ON h.usuario_id = u.usuario_id
+GROUP BY usuario
+ORDER BY usuario ASC;
